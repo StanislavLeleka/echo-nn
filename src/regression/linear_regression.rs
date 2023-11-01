a7fedfadd9edae081144f6d3f9e1b08286f98de6
Stanislav Leleka <stanislav.leleka@gmail.com>
2023-11-02 01:55:22 +0200

    lr fix

--- src/regression/linear_regression.rs
+++ src/regression/linear_regression.rs
@@ -1,6 +1,11 @@
 use ndarray::{s, Array2, Axis};
-use ndarray_rand::rand_distr::Normal;
-use rand::{prelude::Distribution, seq::SliceRandom};
+use ndarray_rand::{rand_distr::Normal, RandomExt};
+use plotters::{
+    prelude::{BitMapBackend, Circle, EmptyElement, IntoDrawingArea, PathElement},
+    series::{LineSeries, PointSeries},
+    style::{Color, IntoFont, ShapeStyle, BLUE, RED, WHITE},
+};
+use rand::{distributions::Standard, prelude::Distribution, seq::SliceRandom};
 use std::{cmp::min, collections::HashMap};
 
 pub struct LinearRegression {}
@@ -29,7 +34,7 @@ impl LinearRegression {
         let p = n.clone() + &weights["b"];
 
         // Compute the loss.
-        let loss = (y_batch - &p).mapv(|x| x.powi(2)).sum() / (2.0 * x_batch.shape()[0] as f64);
+        let loss = (y_batch - &p).mapv(|x| x.powi(2)).sum() / y_batch.dim().0 as f64;
 
         // Save the forward pass variables.
         let mut forward_pass_variables = HashMap::new();
@@ -48,76 +53,74 @@ impl LinearRegression {
     ) -> HashMap<String, Array2<f64>> {
         // Compute dLdW and dLdB for the step-by-step linear regression model.
 
-        let dLdP = -2.0 * (&forward_pass_variables["y_batch"] - &forward_pass_variables["p"]);
+        let dl_dp = -2.0 * (&forward_pass_variables["y_batch"] - &forward_pass_variables["p"]);
 
-        // Compute gradient with respect to W.
-        let dLdW = forward_pass_variables["x_batch"].t().dot(&dLdP);
+        let dp_dn = Array2::from_elem(dl_dp.dim(), 1.0);
+        let dl_dn = dl_dp.clone() * &dp_dn;
+
+        let dl_dw = forward_pass_variables["x_batch"].t().dot(&dl_dn);
 
         // Compute gradient with respect to b.
         // Sum over all examples because b is a scalar.
-        let dLdB_sum = dLdP.sum();
-        let dLdB = Array2::from_elem((1, 1), dLdB_sum);
+        let dl_db_1d = dl_dp.sum_axis(Axis(0));
+        let dl_db = dl_db_1d.insert_axis(Axis(1)); // Convert to 2D array
 
         let mut gradients = HashMap::new();
-        gradients.insert("dLdW".to_string(), dLdW);
-        gradients.insert("dLdB".to_string(), dLdB);
+        gradients.insert("w".to_string(), dl_dw);
+        gradients.insert("b".to_string(), dl_db);
 
-        return gradients;
+        gradients
     }
 
-    fn init_weights(&self, n_in: u32) -> HashMap<String, Array2<f64>> {
+    fn init_weights(&self, n_in: usize) -> HashMap<String, Array2<f64>> {
         // Initialize weights for the step-by-step linear regression model.
 
-        let normal = Normal::new(0.0, 1.0).unwrap();
+        let w = Array2::from_elem((n_in, 1), 0.0);
+        let b = Array2::from_elem((1, 1), 0.0);
 
-        let w = Array2::from_shape_fn((n_in as usize, 1), |_| {
-            normal.sample(&mut rand::thread_rng())
-        });
-        let b = Array2::from_shape_fn((1, 1), |_| normal.sample(&mut rand::thread_rng()));
+        let mut weights = HashMap::new();
+        weights.insert("w".to_string(), w);
+        weights.insert("b".to_string(), b);
 
-        let mut weights = HashMap::with_capacity(2);
-        weights.insert("W".to_string(), w);
-        weights.insert("B".to_string(), b);
-
-        return weights;
+        weights
     }
 
-    fn permute_data(&self, X: &Array2<f64>, y: &Array2<f64>) -> (Array2<f64>, Array2<f64>) {
-        assert_eq!(X.dim().0, y.dim().0);
+    fn permute_data(&self, x: &Array2<f64>, y: &Array2<f64>) -> (Array2<f64>, Array2<f64>) {
+        assert_eq!(x.dim().0, y.dim().0);
 
         // Create a random permutation of indices.
-        let mut indices: Vec<usize> = (0..X.dim().0).collect();
+        let mut indices: Vec<usize> = (0..x.dim().0).collect();
         indices.shuffle(&mut rand::thread_rng());
 
         // Permute the rows of X and y using the permutation.
-        let X_permuted = X.select(Axis(0), &indices);
+        let x_permuted = x.select(Axis(0), &indices);
         let y_permuted = y.select(Axis(0), &indices);
 
-        (X_permuted, y_permuted)
+        (x_permuted, y_permuted)
     }
 
     fn generate_batch(
         &self,
-        X: Array2<f64>,
+        x: Array2<f64>,
         y: Array2<f64>,
         start: usize,
         batch_size: usize,
     ) -> (Array2<f64>, Array2<f64>) {
         // Generate a batch of data.
 
-        assert_eq!(X.dim().0, y.dim().0);
+        assert_eq!(x.dim().0, y.dim().0);
 
-        let end = min(start + batch_size, X.dim().0);
+        let end = min(start + batch_size, x.dim().0);
 
-        let X_batch = X.slice(s![start..end, ..]).to_owned();
+        let x_batch = x.slice(s![start..end, ..]).to_owned();
         let y_batch = y.slice(s![start..end, ..]).to_owned();
 
-        (X_batch, y_batch)
+        (x_batch, y_batch)
     }
 
     fn train(
         &self,
-        X: Array2<f64>,
+        x: Array2<f64>,
         y: Array2<f64>,
         epochs: u32,
         learning_rate: f64,
@@ -126,10 +129,10 @@ impl LinearRegression {
         // Train the model.
 
         // Initialize weights.
-        let mut weights = self.init_weights(X.shape()[1] as u32);
+        let mut weights = self.init_weights(x.shape()[1] as usize);
 
         // Permute data.
-        let (mut X_permuted, mut y_permuted) = self.permute_data(&X, &y);
+        let (mut x_permuted, mut y_permuted) = self.permute_data(&x, &y);
 
         let mut losses = Vec::new();
 
@@ -137,22 +140,22 @@ impl LinearRegression {
         let mut start = 0;
         for _ in 0..epochs {
             // Generate a batch.
-            let (X_batch, y_batch) =
-                self.generate_batch(X_permuted.clone(), y_permuted.clone(), start, batch_size);
+            let (x_batch, y_batch) =
+                self.generate_batch(x_permuted.clone(), y_permuted.clone(), start, batch_size);
 
             start += batch_size;
 
-            if start >= X_permuted.dim().0 {
+            if start >= x_permuted.dim().0 {
                 // Permute data for the next epoch.
-                let permuted = self.permute_data(&X, &y);
-                X_permuted = permuted.0;
+                let permuted = self.permute_data(&x, &y);
+                x_permuted = permuted.0;
                 y_permuted = permuted.1;
                 start = 0;
             }
 
             // Compute the forward pass.
             let (loss, forward_pass_variables) =
-                self.forward_linear_regression(&X_batch, &y_batch, weights.clone());
+                self.forward_linear_regression(&x_batch, &y_batch, weights.clone());
 
             losses.push(loss);
 
@@ -168,14 +171,204 @@ impl LinearRegression {
 
         (losses, weights)
     }
+
+    fn predict(X: &Array2<f64>, weights: HashMap<String, Array2<f64>>) -> Array2<f64> {
+        // Predict using the step-by-step linear regression model.
+
+        let n = X.dot(&weights["w"]);
+        let p = n.clone() + &weights["b"];
+
+        p
+    }
+
+    fn mae(y_true: &Array2<f64>, y_pred: &Array2<f64>) -> f64 {
+        // Compute the mean absolute error.
+
+        assert_eq!(y_true.dim(), y_pred.dim());
+
+        let mae = (y_true - y_pred).mapv(|x| x.abs()).sum() / y_true.dim().0 as f64;
+
+        mae
+    }
+
+    fn rmse(y_true: &Array2<f64>, y_pred: &Array2<f64>) -> f64 {
+        // Compute the root mean squared error.
+
+        assert_eq!(y_true.dim(), y_pred.dim());
+
+        let rmse = ((y_true - y_pred).mapv(|x| x.powi(2)).sum() / y_true.dim().0 as f64).sqrt();
+
+        rmse
+    }
+
+    fn plot_losses(losses: &Vec<f64>) {
+        let root =
+            BitMapBackend::new("src/data/linear_regression.png", (800, 600)).into_drawing_area();
+        root.fill(&WHITE).unwrap();
+
+        let max_loss = *losses
+            .iter()
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap_or(&0.0);
+        let min_loss = *losses
+            .iter()
+            .min_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap_or(&0.0);
+
+        let mut chart = plotters::chart::ChartBuilder::on(&root)
+            .caption("Training Loss Over Epochs", ("sans-serif", 20).into_font())
+            .margin(10)
+            .x_label_area_size(40)
+            .y_label_area_size(40)
+            .build_cartesian_2d(0..losses.len(), min_loss..max_loss)
+            .unwrap();
+
+        chart.configure_mesh().draw().unwrap();
+
+        chart
+            .draw_series(LineSeries::new(
+                losses.iter().enumerate().map(|(x, y)| (x, *y)),
+                &RED,
+            ))
+            .unwrap()
+            .label("Loss")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+        chart.configure_series_labels().draw().unwrap();
+    }
+
+    fn plot_predictions(x: &Array2<f64>, y_true: &Array2<f64>, y_pred: &Array2<f64>) {
+        let root = BitMapBackend::new("src/data/linear_regression_predictions.png", (800, 600))
+            .into_drawing_area();
+        root.fill(&WHITE).unwrap();
+
+        let x_values: Vec<f64> = x.column(0).to_vec();
+        let y_true_values: Vec<f64> = y_true.column(0).to_vec();
+        let y_pred_values: Vec<f64> = y_pred.column(0).to_vec();
+
+        println!("x_values: {:?}", x_values);
+        println!("y_true_values: {:?}", y_true_values);
+        println!("y_pred_values: {:?}", y_pred_values);
+
+        let x_min = x
+            .iter()
+            .min_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap_or(&0.0)
+            - 5.0;
+        let x_max = x
+            .iter()
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap_or(&0.0)
+            + 5.0;
+        let y_min = y_true
+            .iter()
+            .min_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap_or(&0.0)
+            - 5.0;
+        let y_max = y_true
+            .iter()
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .unwrap_or(&0.0)
+            + 5.0;
+
+        let mut chart = plotters::chart::ChartBuilder::on(&root)
+            .caption("True vs Predicted", ("sans-serif", 40))
+            .margin(5)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
+            .build_cartesian_2d(x_min..x_max, y_min..y_max)
+            .unwrap();
+
+        chart.configure_mesh().draw().unwrap();
+
+        chart
+            .draw_series(
+                x_values
+                    .iter()
+                    .zip(&y_true_values)
+                    .map(|(&x, &y)| Circle::new((x, y), 5, BLUE.filled())),
+            )
+            .unwrap();
+
+        chart
+            .draw_series(LineSeries::new(
+                x_values.iter().zip(&y_pred_values).map(|(&x, &y)| (x, y)),
+                &RED,
+            ))
+            .unwrap();
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use ndarray::array;
+    use ndarray::{array, Array2};
     use std::collections::HashMap;
 
     use super::LinearRegression;
+
+    #[test]
+    fn test_linear_regression() {
+        let (x, y) = read_salary_dataset();
+
+        let linear_regression = LinearRegression::new();
+
+        let (losses, weights) = linear_regression.train(x.clone(), y.clone(), 50, 0.0001, 32);
+
+        println!("weights: {:?}", weights);
+
+        let y_pred = LinearRegression::predict(&x, weights);
+
+        let mae = LinearRegression::mae(&y, &y_pred);
+        let rmse = LinearRegression::rmse(&y, &y_pred);
+
+        println!("x: {:?}", x);
+        println!("y: {:?}", y);
+        println!("y_pred: {:?}", y_pred);
+
+        println!("MAE: {}", mae);
+        println!("RMSE: {}", rmse);
+
+        LinearRegression::plot_losses(&losses);
+        LinearRegression::plot_predictions(&x, &y, &y_pred);
+    }
+
+    #[test]
+    fn test_linear_regression2() {
+        let x = Array2::from_shape_vec((50, 1), (0..50).map(|x| x as f64).collect()).unwrap();
+        let y = &x * 2.0 + 5.0; // Simple linear relation
+        let model = LinearRegression::new();
+        let (losses, weights) = model.train(x.clone(), y.clone(), 200, 0.0001, 10);
+        let y_pred = LinearRegression::predict(&x, weights);
+        let mae = LinearRegression::mae(&y, &y_pred);
+        let rmse = LinearRegression::rmse(&y, &y_pred);
+
+        println!("mae: {}", mae);
+        println!("rmse: {}", rmse);
+
+        LinearRegression::plot_predictions(&x, &y, &y_pred);
+        LinearRegression::plot_losses(&losses);
+    }
+
+    fn read_salary_dataset() -> (Array2<f64>, Array2<f64>) {
+        let mut rdr = csv::Reader::from_path("src/data/salary_dataset.csv").unwrap();
+
+        let mut data = Vec::new();
+        let mut target = Vec::new();
+
+        for result in rdr.records() {
+            let record = result.unwrap();
+            let x = record[1].parse::<f64>().unwrap();
+            let y = record[2].parse::<f64>().unwrap();
+
+            data.push(x);
+            target.push(y);
+        }
+
+        let data_array = Array2::from_shape_vec((data.len(), 1), data).unwrap();
+        let target_array = Array2::from_shape_vec((target.len(), 1), target).unwrap();
+
+        (data_array, target_array)
+    }
 
     #[test]
     fn test_forward_linear_regression() {
@@ -210,7 +403,7 @@ mod tests {
         let gradients = linear_regression.loss_gradient(forward_pass_variables, weights);
 
         let tolerance = 1e-5;
-        assert!(gradients["dLdW"].mapv(|x| x.abs()).sum() < tolerance);
-        assert!(gradients["dLdB"].mapv(|x| x.abs()).sum() < tolerance);
+        assert!(gradients["w"].mapv(|x| x.abs()).sum() < tolerance);
+        assert!(gradients["b"].mapv(|x| x.abs()).sum() < tolerance);
     }
 }
